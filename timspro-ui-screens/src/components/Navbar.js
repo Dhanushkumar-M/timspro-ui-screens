54e5493756b5c1efdb468aafeb24dbbfab5bbeef
Dhanushkumar_M <dhanushwinners007@gmail.com>
2020-09-25 19:29:29 +0530

    Initial Sidebar commit

---
+++ timspro-ui-screens/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './Sidebardata';
+
+function Navbar() {
+  return (
+    <>
+        <div className='navbar'>
+            <h1>Homepage</h1>
+        </div>
+        <nav className='nav-menu active'>
+          <ul className='nav-menu-items'>
+            <li className='navbar-toggle'>
+                <h1 className='nav_title'>TIMS</h1>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {/* {item.icon} */}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+         </ul>
+         
+        </nav>
+    </>
+  );
+}
+
+export default Navbar;

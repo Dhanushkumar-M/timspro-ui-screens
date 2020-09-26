b8ed83f50f7018f318f4374d982954ff05da375f
Sanju <49448546+Sanju-S-S@users.noreply.github.com>
2020-09-26 08:40:21 +0530

    commit on 'sanju_branch'

---
+++ timspro-ui-screens/src/pages/Landingpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LpTopNav from '../components/LpTopNav';
+
+
+function Landingpage() {
+    return (
+      <div>
+        <LpTopNav/>
+      </div>
+    );
+  }
+
+ export default Landingpage;
